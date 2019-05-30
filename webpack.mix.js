13fd98f0924ba27f641db9cc7488e03f5fc623fe
Ryan C. Durham <ryan@stagerightlabs.com>
2019-05-29 22:15:44 -0700

    Add compiled asset versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,6 @@
 const mix = require('laravel-mix');
 require('mix-tailwindcss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.sass('resources/sass/app.scss', 'public/css')
-   .tailwind();
+   .tailwind()
+   .version();
