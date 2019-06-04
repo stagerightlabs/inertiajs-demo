5c1ae3f7997d48376f8ce30404c2c97d0f30f3f7
Ryan C. Durham <ryan@stagerightlabs.com>
2019-06-03 21:00:20 -0700

    Install Zondicons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
+import Zondicon from 'vue-zondicons';
 import Inertia from 'inertia-vue'
 import Vue from 'vue'
 
 Vue.mixin({ methods: { route: (...args) => window.route(...args).url() } })
 Vue.use(Inertia)
+Vue.component('Zondicon', Zondicon);
 
 const app = document.getElementById('app')
 
